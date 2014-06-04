25c39ee5603cf66d15a1b9b60aaf90c5a66108a9
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-04 20:27:46 +0200

    guard with Reject

--- src/de/buerkingo/billiards/game/straight/StraightPoolEvent.java
+++ src/de/buerkingo/billiards/game/straight/StraightPoolEvent.java
@@ -3,6 +3,7 @@ package de.buerkingo.billiards.game.straight;
 import java.io.Serializable;
 
 import de.buerkingo.billiards.game.GameEvent;
+import de.buerkingo.billiards.util.reject.Reject;
 
 /**
  * Represents one set of information to be handled by the game logic.
@@ -18,12 +19,17 @@ public class StraightPoolEvent implements GameEvent, Serializable {
 
     /** The number of balls that are left on the table after this event. */
     public StraightPoolEvent withNumberOfBallsLeftInRack( int numberOfBallsLeftInRack ) {
+        Reject.ifLessThan( numberOfBallsLeftInRack, 0 );
+        Reject.ifGreaterThan( numberOfBallsLeftInRack, 15 );
+
         this.numberOfBallsLeftInRack = numberOfBallsLeftInRack;
         return this;
     }
 
     /** The number of racks that were finished during this event. This equals the number of re-racks that took place. */
     public StraightPoolEvent withNumberOfFinishedRacks( int numberOfFinishedRacks ) {
+        Reject.ifLessThan( numberOfFinishedRacks, 0 );
+
         this.numberOfFinishedRacks = numberOfFinishedRacks;
         return this;
     }

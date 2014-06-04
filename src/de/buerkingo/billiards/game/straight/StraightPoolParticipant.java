98a0e6ed11d210d4a8f8b4b6bdac63dfee242d80
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-04 17:42:08 +0200

    added a participant class specifically for straight pool

---
+++ src/de/buerkingo/billiards/game/straight/StraightPoolParticipant.java
@@ -0,0 +1,10 @@
+package de.buerkingo.billiards.game.straight;
+
+import de.buerkingo.billiards.participants.Participant;
+
+/**
+ * Represents a participant of a straight pool match.
+ */
+public class StraightPoolParticipant implements Participant {
+
+}

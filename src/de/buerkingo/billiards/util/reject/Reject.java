1860f884aad8cc80c6be98f4df43b3dca6a9ced6
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-04 20:30:47 +0200

    added ifNull and fixed default messages

--- src/de/buerkingo/billiards/util/reject/Reject.java
+++ src/de/buerkingo/billiards/util/reject/Reject.java
@@ -4,8 +4,18 @@ import static java.lang.String.format;
 
 public class Reject {
 
+    public static void ifNull( Object obj ) {
+        ifNull( "expected parameter not to be null", obj );
+    }
+
+    public static void ifNull( String description, Object obj ) {
+        if( obj == null ) {
+            reject( description );
+        }
+    }
+
     public static void ifLessThan( int actual, int expected ) {
-        ifLessThan( format( "expected %d to be less than %d", actual, expected ), actual, expected );
+        ifLessThan( format( "expected %d not to be less than %d", actual, expected ), actual, expected );
     }
 
     public static void ifLessThan( String description, int actual, int expected ) {
@@ -15,7 +25,7 @@ public class Reject {
     }
 
     public static void ifGreaterThan( int actual, int expected ) {
-        ifGreaterThan( format( "expected %d to be greater than %d", actual, expected ), actual, expected );
+        ifGreaterThan( format( "expected %d not to be greater than %d", actual, expected ), actual, expected );
     }
 
     public static void ifGreaterThan( String description, int actual, int expected ) {

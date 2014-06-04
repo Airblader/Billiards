c3659a1bbe16a1fbc5b8d8a64eb6f9f865d20a53
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-04 17:58:31 +0200

    added number of balls left in rack and number of finished racks

--- src/de/buerkingo/billiards/game/straight/StraightPoolGameEvent.java
+++ src/de/buerkingo/billiards/game/straight/StraightPoolGameEvent.java
@@ -11,4 +11,29 @@ public class StraightPoolGameEvent implements GameEvent, Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int numberOfBallsLeftInRack;
+    private int numberOfFinishedRacks;
+
+    private StraightPoolGameEvent() {}
+
+    /** The number of balls that are left on the table after this event. */
+    public StraightPoolGameEvent withNumberOfBallsLeftInRack( int numberOfBallsLeftInRack ) {
+        this.numberOfBallsLeftInRack = numberOfBallsLeftInRack;
+        return this;
+    }
+
+    /** The number of racks that were finished during this event. This equals the number of re-racks that took place. */
+    public StraightPoolGameEvent withNumberOfFinishedRacks( int numberOfFinishedRacks ) {
+        this.numberOfFinishedRacks = numberOfFinishedRacks;
+        return this;
+    }
+
+    public int getNumberOfBallsLeftInRack() {
+        return numberOfBallsLeftInRack;
+    }
+
+    public int getNumberOfFinishedRacks() {
+        return numberOfFinishedRacks;
+    }
+
 }

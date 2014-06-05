96fb3b478e1bb4c978588172a9aabe713c0514f2
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-05 07:55:13 +0200

    added some reject methods

--- src/de/buerkingo/billiards/util/reject/Reject.java
+++ src/de/buerkingo/billiards/util/reject/Reject.java
@@ -2,6 +2,8 @@ package de.buerkingo.billiards.util.reject;
 
 import static java.lang.String.format;
 
+import java.util.List;
+
 public class Reject {
 
     public static void ifNull( Object obj ) {
@@ -10,6 +12,36 @@ public class Reject {
 
     public static void ifNull( String description, Object obj ) {
         if( obj == null ) {
+            reject( description );
+        }
+    }
+
+    public static void ifNotNull( Object obj ) {
+        ifNotNull( "expected parameter to be null", obj );
+    }
+
+    public static void ifNotNull( String description, Object obj ) {
+        if( obj != null ) {
+            reject( description );
+        }
+    }
+
+    public static void ifEqual( int actual, int expected ) {
+        ifEqual( format( "expected %d not to be equal to %d", actual, expected ), actual, expected );
+    }
+
+    public static void ifEqual( String description, int actual, int expected ) {
+        if( actual == expected ) {
+            reject( description );
+        }
+    }
+
+    public static void ifNotEqual( int actual, int expected ) {
+        ifNotEqual( format( "expected %d to be equal to %d", actual, expected ), actual, expected );
+    }
+
+    public static void ifNotEqual( String description, int actual, int expected ) {
+        if( actual != expected ) {
             reject( description );
         }
     }
@@ -30,6 +62,28 @@ public class Reject {
 
     public static void ifGreaterThan( String description, int actual, int expected ) {
         if( actual > expected ) {
+            reject( description );
+        }
+    }
+
+    public static void ifEmpty( List<?> list ) {
+        ifEmpty( "expected not to be empty", list );
+    }
+
+    public static void ifEmpty( String description, List<?> list ) {
+        ifNull( list );
+        if( list.isEmpty() ) {
+            reject( description );
+        }
+    }
+
+    public static void ifNotEmpty( List<?> list ) {
+        ifNotEmpty( "expected to be empty", list );
+    }
+
+    public static void ifNotEmpty( String description, List<?> list ) {
+        ifNull( list );
+        if( !list.isEmpty() ) {
             reject( description );
         }
     }

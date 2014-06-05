a6bcc1b36971c1b6bdf51961ddf0190d5f6bba07
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-05 07:23:13 +0200

    added a list of participants

--- src/de/buerkingo/billiards/participants/Participants.java
+++ src/de/buerkingo/billiards/participants/Participants.java
@@ -1,6 +1,12 @@
 package de.buerkingo.billiards.participants;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.format;
+
 import java.io.Serializable;
+import java.util.List;
+
+import de.buerkingo.billiards.util.reject.Reject;
 
 /**
  * Holds and manages the list of participants in a game.
@@ -9,4 +15,14 @@ public class Participants implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private final int numberOfParticipants = 2;
+    private final List<Participant> participants = newArrayList();
+
+    public Participant get( int participantIndex ) {
+        Reject.ifLessThan( participantIndex, 0 );
+        Reject.ifGreaterThan( format( "cannot get participant #%d, there are only %d participants", participantIndex, numberOfParticipants ),
+            participantIndex, numberOfParticipants - 1 );
+
+        return participants.get( participantIndex );
+    }
 }

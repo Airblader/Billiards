9566b289f080684d1547209bc0f36d307fc6bb04
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-08 23:00:02 +0200

    don't process many events

--- src/de/buerkingo/billiards/game/straight/StraightPoolGame.java
+++ src/de/buerkingo/billiards/game/straight/StraightPoolGame.java
@@ -1,17 +1,15 @@
 package de.buerkingo.billiards.game.straight;
 
-import static com.google.common.collect.Iterables.filter;
-
 import java.io.Serializable;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
 
 import de.buerkingo.billiards.game.Game;
 import de.buerkingo.billiards.game.GameEvent;
 import de.buerkingo.billiards.game.straight.events.FinishedInningEvent;
 import de.buerkingo.billiards.game.straight.events.FinishedRackEvent;
 import de.buerkingo.billiards.game.straight.events.FoulEvent;
+import de.buerkingo.billiards.game.straight.foul.Foul;
 import de.buerkingo.billiards.participants.Participants;
 import de.buerkingo.billiards.util.reject.Reject;
 
@@ -33,50 +31,48 @@ public class StraightPoolGame implements Game<StraightPoolParticipant, StraightP
         this.maxInnings = maxInnings;
     }
 
-    public StraightPoolState processEvents( GameEvent... rawEvents ) {
-        Reject.ifNull( rawEvents );
+    public StraightPoolState processEvents( GameEvent rawEvent, Optional<Foul> foul ) {
+        Reject.ifNull( rawEvent );
+        Reject.ifNull( foul );
 
         StraightPoolParticipant participant = participants.getActiveParticipant();
 
-        boolean containsFoul = filter( Lists.newArrayList( rawEvents ), FoulEvent.class ).iterator().hasNext();
         boolean controlPasses = false;
 
-        for( GameEvent rawEvent : rawEvents ) {
-            if( rawEvent instanceof FinishedInningEvent ) {
-                FinishedInningEvent event = (FinishedInningEvent) rawEvent;
-                Reject.ifGreaterThan( "there cannot be more balls left than before",
-                    event.getNumberOfRemainingBalls(), rack.getCurrentNumberOfBalls() );
+        if( rawEvent instanceof FinishedInningEvent ) {
+            FinishedInningEvent event = (FinishedInningEvent) rawEvent;
+            Reject.ifGreaterThan( "there cannot be more balls left than before",
+                event.getNumberOfRemainingBalls(), rack.getCurrentNumberOfBalls() );
 
-                int pocketedBalls = rack.getCurrentNumberOfBalls() - event.getNumberOfRemainingBalls();
-                if( pocketedBalls > 0 ) {
-                    participant.getInning().addPoints( pocketedBalls );
-                    participant.resetConsecutiveFouls();
-                }
-
-                if( event.endedWithSafety() ) {
-                    participant.getInning().endedWithSafety();
-                    controlPasses = true;
-                }
-
-                // TODO refine logic
-                if( event.getNumberOfRemainingBalls() > 1 ) {
-                    controlPasses = true;
-                }
-
-                // TODO advance inning
-            } else if( rawEvent instanceof FinishedRackEvent ) {
-                FinishedRackEvent event = (FinishedRackEvent) rawEvent;
-
-                // TODO handle event
-            } else if( rawEvent instanceof FoulEvent ) {
-                FoulEvent event = (FoulEvent) rawEvent;
-
-                controlPasses = true;
-
-                // TODO handle event
-            } else {
-                Reject.always( "unknown event type" );
+            int pocketedBalls = rack.getCurrentNumberOfBalls() - event.getNumberOfRemainingBalls();
+            if( pocketedBalls > 0 ) {
+                participant.getInning().addPoints( pocketedBalls );
+                participant.resetConsecutiveFouls();
             }
+
+            if( event.endedWithSafety() ) {
+                participant.getInning().endedWithSafety();
+                controlPasses = true;
+            }
+
+            // TODO refine logic
+            if( event.getNumberOfRemainingBalls() > 1 ) {
+                controlPasses = true;
+            }
+
+            // TODO advance inning
+        } else if( rawEvent instanceof FinishedRackEvent ) {
+            FinishedRackEvent event = (FinishedRackEvent) rawEvent;
+
+            // TODO handle event
+        } else if( rawEvent instanceof FoulEvent ) {
+            FoulEvent event = (FoulEvent) rawEvent;
+
+            controlPasses = true;
+
+            // TODO handle event
+        } else {
+            Reject.always( "unknown event type" );
         }
 
         if( controlPasses ) {

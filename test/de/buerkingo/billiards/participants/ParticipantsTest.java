4b56af2595eb05a947257e6cd8768be834b6bdf5
Ingo Bürk <ingo.buerk@tngtech.com>
2014-06-09 07:49:31 +0200

    added test for idempotency

--- test/de/buerkingo/billiards/participants/ParticipantsTest.java
+++ test/de/buerkingo/billiards/participants/ParticipantsTest.java
@@ -27,6 +27,20 @@ public class ParticipantsTest {
             .isEqualTo( PLAYER_A );
     }
 
+    @Test
+    public void testGetInningIdempotency() {
+        Participants<StraightPoolParticipant> participants = new Participants<StraightPoolParticipant>()
+            .setParticipants( createParticipant( PLAYER_A ), createParticipant( PLAYER_B ) );
+
+        assertThat( participants.getActiveParticipant().getInning().getNumber() ).isEqualTo( 1 );
+        assertThat( participants.getActiveParticipant().getInning().getNumber() ).isEqualTo( 1 );
+
+        participants.getActiveParticipant().getInning().end();
+
+        assertThat( participants.getActiveParticipant().getInning().getNumber() ).isEqualTo( 2 );
+        assertThat( participants.getActiveParticipant().getInning().getNumber() ).isEqualTo( 2 );
+    }
+
     private StraightPoolParticipant createParticipant( String name ) {
         return new StraightPoolParticipant( new Person( name ) );
     }
